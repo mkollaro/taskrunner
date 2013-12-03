151f9fcc656672082aa878b4cf4591838291ccfc
Martina Kollarova <mkollaro@redhat.com>
2013-12-03 16:00:21 +0100

    Updated setup.py into version 0.1
    Temporarily remove the long_description, docutils were having some problems
    with it.

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='taskrunner',
-    version='0.0.1',
+    version='0.1',
     author='Martina Kollarova',
     author_email='mkollaro@gmail.com',
     url='http://pypi.python.org/pypi/taskrunner/',
     packages=['taskrunner'],
-    license='LICENSE',
+    license='Apache License, Version 2.0',
     scripts=['bin/taskrunner'],
     description='Execute a certain sequence of tasks and later their'
                 ' cleanups in reverse order.',
-    long_description=open('README.md').read(),
-    install_requires=[],
 )
